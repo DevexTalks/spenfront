9ec243ef5e43e880ce7cd4140d04842e6043934c
Hernando Silva Leal <hernandosilvaleal@gmail.com>
2023-06-24 22:51:11 -0500

    enviroment local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://localhost:5000',
   // apiUrl: 'http://34.121.2.93:8080',
-  apiUrl: 'https://back.lupaapsalud.com',
+  // apiUrl: 'https://back.lupaapsalud.com',
 };
